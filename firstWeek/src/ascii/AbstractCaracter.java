56e33bef79ebba9ad71de4a6c41ca312ebab6a00
GaelleChmargounof <gaelle.chmargounof@gmail.com>
2017-01-16 17:37:43 +0100

    Create AbstractCaracter.java

---
+++ firstWeek/src/ascii/AbstractCaracter.java
@@ -0,0 +1,14 @@
+package ascii;
+
+public abstract class AbstractCaracter
+{
+   
+       
+    protected char [][] definition;
+
+    public char[][] toAscii()
+    {
+        return this.definition;
+    }
+
+}
